bb8e66d52d8722f532052667a26f08b6d451e743
Glitch (zealous-quapo) <none>
2017-11-16 15:53:57 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-
+app.use(express.static(__dirname + '/public'));
 router.get('/', function(req, res) {
      res.sendFile(__dirname + '/views/index.html');
 });
