ff6ffbd2b87dee204795517159ae2210418069ff
waxmenow <33286500+waxmenow@users.noreply.github.com>
2017-11-17 16:11:42 -0500

    Update server.js

--- server.js
+++ server.js
@@ -40,46 +40,40 @@ app.use(session({
   secret: 'my-name-is-ozymandias-king-of-kings', 
   cookie: { maxAge: 60000 },
   proxy   : 'true',
-   secure: false
+  secure: false,
  //store   : new MemcachedStore({
- //       hosts: ['waxmenowmem.24l8cc.0001.use1.cache.amazonaws.com:11211'], //this should be where your Memcached server is running
- //      secret: 'memcached-secret-key'
-//     username: 'F53955',
- //   password: '73AAAEEECFF4C7A65977B169C76FEF2F'
-// })
+      //  hosts: ['waxmenowmem.24l8cc.0001.use1.cache.amazonaws.com:11211'], 
+ //  hosts: ['mc3.dev.ec2.memcachier.com:11211'], //this should be where your Memcached server is running
+  //    secret: 'memcached-secret-key',
+ //   username: 'F53955',
+  // password: '73AAAEEECFF4C7A65977B169C76FEF2F'
+//})
 }));
  
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+
 app.use(express.static(__dirname + '/public'));
+
 router.get('/', function(req, res) {
      res.sendFile(__dirname + '/views/index.html');
 });
 
+
 router.get('/acceptAppointment', function(req, res) {
-  console.log("inside acceptAppointment for record ID:" + req.query.appointmentID);
+     console.log("****INSIDE REGISTER USER*****")
+     console.log("**** session ID= " + req.sessionID) 
+     console.log("req.query.appointmentID= " + req.query.appointmentID)
   
   base('appointment').find(req.query.appointmentID, function(err, record) {
     if (err) { console.error(err); return; }
-    console.log(record);
+    console.log(record)
+  });
 
-    
-});
- // base('appointment').
-  
-   /*  res.writeHead(200, "OK", {'Content-Type': 'text/html'});
-          res.write('<html><head><title>Hello</title></head><body>');
-          res.write('<h1>Welcomeo are you?</h1>');
-          res.write('<form="application/x-www-form-urlencoded" action="/confirmAppointment" method="POST">');
-          res.write('<input type="text" label="enter time you will do this appointment" name="username" value="John Doe" /><br />');
-          res.write('<input type="text" name="userage" value="99" /><br />');
-          res.write('<input type="text" hidden="true" name="appointmentID" value=' + req.query.appointmentID + '>')
-          res.write('<button type="submit" value="Submit">Submit</button>');
-          res.write('</form></body></html>');
-          res.end();*/
-  console.log("from acceptAppointment appointmentID: " +req.query.appointmentID);
+  console.log("from acceptAppointment appointmentID: " + req.query.appointmentID);
   
   res.writeHead(200, "OK", {'Content-Type': 'text/html'});
   res.write('<form action="/confirmAppointment" method="get">');
@@ -104,9 +98,17 @@ router.get('/confirmAppointment', function(req, res) {
   res.send("goobers");
 });
             
-router.post('/registeruser', function(req, res) {
-    console.log("****INSIDE REGISTER USER*****");
+app.use('/', router);
 
+
+
+
+
+
+router.post('/registeruser', function(req, res) {
+    console.log("****INSIDE REGISTER USER*****")
+    console.log("**** session ID= " + req.sessionID)
+  
     base('member').create({
         "firstName": req.body.firstName,
         "lastName": req.body.lastName,
@@ -120,154 +122,51 @@ router.post('/registeruser', function(req, res) {
             console.error(err);
             return;
         }
-       console.log("req.body.emailAddress: " + req.body.emailAddress);
-       // console.log("Member record created by Airtable= " + record.getId());
-        mc.set('fullName', (req.body.firstName + " " + req.body.lastName), 'expires:20000');
-      //  console.log("fullName set to: ", req.body.firstName + " " + req.body.lastName);
-        mc.set('memberRecordId', record.getId(), 'expires:20000');
-        req.session.memberRecordID = record.getId();
-      console.log(req.sessionID);
       
-  res.send("acceptAppointment");
+      req.session.memberRecordID = record.getId()
+      console.log("req.session.memberRecordID " + req.session.memberRecordID)
+  
+      
+      res.send("registeruser Complete")
 
     });
 });
 
-
-
-/*router.post('/takeAppointment', function(req, res) {
-    console.log("****INSIDE REGISTER USER*****");
-
-    base('appointment').update({
-        // add code to update AIRTABLE with name of waxer accepting appointment and time of acceptance
-
-    }, function(err, record) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-
-
-
-    });
-});*/
-
-
-
-function getmcValue(resultIndex, val, callback, p1,p2,p3) {
-    //console.log("callback sent to getmcValue: " + callback);
-    //console.log("val sent to getmcValue " + val);
-
-    mc.get(val, function(err, value, key) {
-        if (value != null) {
-            console.log("*****INSIDE getmcValue --- value.toString()= " + value.toString());
-        }
-        result[resultIndex] = value.toString();
-        callback(p1,p2,p3);
-    });
-}
-
-function createAppointmentRecord(startDate, availabilityStartTime, availabilityEndTime, Status) { 
-
-    base('appointment').create({
-        "appointmentDate": startDate,
-        "availabilityStartTime": availabilityStartTime,
-        "availabilityEndTime": availabilityEndTime,
+router.post('/createScheduleRecord', function(req, res) {
+    console.log("*****INSIDE SCHEDULE RECORD*****");
+    console.log("**** session ID= " + req.sessionID)
+  
+  base('appointment').create({
+        "appointmentDate": (Date.parse(req.body.startDate)),
+        "availabilityStartTime": (Date.parse(req.body.startDate + " " + req.body.startTime + " GMT-0500")),
+        "availabilityEndTime": Date.parse(req.body.startDate + " " + req.body.endTime + " GMT-0500"),
         "Status": "Awaiting Assignment",
       // group set to MIAMI record ID.. extend later for expansion.
         "group": [ "recy8zny8oO3X0mt2" ]
     
     }, function(err, record) {
         if (err) {
-            console.error(err);
-            return;
+            console.error(err)
+            return
         }
-         mc.get('memberRecordId', function(err, value, key) {
-            if (value != null) {
-                console.log("Schedule Record shows memberRecordId as " + value.toString());
-            }
-            mc.set('appointmentRecordId', record.getId(), 'expires:20000');
 
-        });
-
-        console.log(record.getId());
-
-
-
+        req.session.appointmentRecordID = record.getId()
+        console.log("req.session.appointmentRecordID = " + req.session.appointmentRecordID)
+    
+        res.send("createScheduleRecord Complete")
 
     });
-}
-
-function updateAppointmentRecord(waxes) {
- // console.log('some string:'+ somestring); //comes through undefinted here
-    var servicesRequested = JSON.parse(waxes);
-    console.log('Parsed services: ', servicesRequested);
-  
-    var arrayofWaxes = [];
-
-    var arrayLength = servicesRequested.length;
-    for (var i = 0; i < arrayLength; i++) {
-        //console.log(waxes[i][3]);
-        if (servicesRequested[i][2] == 1) {
-          
-            arrayofWaxes.push(servicesRequested[i][3]);
-         
-        }
-        //Do something
-    }
- console.log('rightbefore update arrayofwaxes: ' + arrayofWaxes.length)
-    base('appointment').update(result[1], {
-           "memberName": [req.session.memberRecordID],
-        "servicesRequested": arrayofWaxes,
-      "appointmentLocation": [result[2]]
-    }, function(err, record) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-
-
-    });
-}
-
-
-
-//===================
-
-
-
-router.post('/createScheduleRecord', function(req, res) {
-
-
-    console.log("*****INSIDE SCHEDULE RECORD*****");
-
-//values work here... the CB gets the parameters.  from the getmcValue below..(updateappointmentrecord) for some reason.. nothing goes through!.. missing something SIMPLE!  (im calling these from a loca
-// html server.. views/index.html is irrelevant)
-    getmcValue(0,'memberRecordId', createAppointmentRecord, (Date.parse(req.body.startDate)), (Date.parse(req.body.startDate + " " + req.body.startTime + " GMT-0500")), Date.parse(req.body.startDate + " " + req.body.endTime + " GMT-0500"));
-
-       console.log('****THE RECORD ID WAS STORED WITH EXPRESS SESSION AND ITS VALUE IS:' +  req.session.memberRecordID);
-      console.log(req.sessionID);
-
-
-  res.send("createScheduleRecord");
-
-});
-
-router.post('/updateServices', function(req, res) {
-    console.log("****** INSIDE /updateServices");
-    console.log("serviceString from HTML POST: " + req.body.serviceString);
-  
-    getmcValue(1,'appointmentRecordId', updateAppointmentRecord, req.body.serviceString,"peanuts","ginandtonic");
-  
-  
-  
-    res.send("updateServices");
 });
 
 router.post('/setLocation', function(req, res) {
-  //test session store of record id compare to result[0] to get rid of memjs
-  console.log("inside of setLocation route, memberRecordID from req.session is: " + req.session.memberRecordID);
-    base('memberLocation').create({
+    console.log("****INSIDE REGISTER USER*****")
+    console.log("**** session ID= " + req.sessionID)
+  
+    console.log('session.appointmentRecordID =' + req.session.appointmentRecordID)
+    console.log('session.memberRecordID =' + req.session.memberRecordID)
+
+
+  base('memberLocation').create({
         "memberName": [req.session.memberRecordID],
         "addressStreet01": req.body.street_number + " " + req.body.route,
         "aptNumber": req.body.aptNumber,
@@ -282,31 +181,64 @@ router.post('/setLocation', function(req, res) {
             console.error(err);
             return;
         }
-        console.log("setLocation called");
+    
+      
         console.log("req.body.streetNumber = " + req.body.street_number);
         console.log("req.body.locality = " + req.body.locality);
-        console.log(record.getId());
-        
-        mc.set('locationRecordId', record.getId(), 'expires:20000');
-      
-         mc.get('locationRecordId', function(err, value, key) {
-            if (value != null) {
-                console.log("locationRecordId= " + value.toString());
-              result[2] = value.toString();
-            }
-          result[2] = record.getId(); 
-        });
-      
-    });
 
-  res.send("setLocation");
+        
+        req.session.locationRecordID = record.getId();
+        
+        res.send("setLocation Complete");
+    });
 });
 
-app.use('/', router);
+router.post('/updateServices', function(req, res) {
+    console.log("****INSIDE REGISTER USER*****")
+    console.log("**** session ID= " + req.sessionID)
+    
+    console.log("serviceString from HTML POST: " + req.body.serviceString);
 
-// we'll create our routes here
+    var servicesRequested = JSON.parse(req.body.serviceString)
+ //   console.log('Parsed services: ', servicesRequested)
+  
+    var arrayofWaxes = []
+
+    var arrayLength = servicesRequested.length
+    for (var i = 0; i < arrayLength; i++) {
+        //console.log(waxes[i][3]);
+        if (servicesRequested[i][2] == 1) {
+          
+            arrayofWaxes.push(servicesRequested[i][3]);
+         
+        }
+    }
+  
+  console.log('number of services' + arrayofWaxes.length)
+  console.log('session.appointmentRecordID =' + req.session.appointmentRecordID)
+  console.log('session.memberRecordID =' + req.session.memberRecordID)
+  
+    base('appointment').update(req.session.appointmentRecordID, {
+           "memberName": [req.session.memberRecordID],
+        "servicesRequested": arrayofWaxes,
+      "appointmentLocation": [req.session.locationRecordID]
+    }, function(err, record) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+
+    });
+  
+    res.send("updateServices Complete")
+});
+
+
+
+
 
 // START THE SERVER
 // ==============================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+app.listen(port)
+console.log('Magic happens on port ' + port)
